205aaeb1104c6c11573c35217cde9d91a5ceb103
Gabriel Pino <126812593+PinoGabriel@users.noreply.github.com>
2024-02-01 15:33:45 +0100

    Initial commit

---
+++ config/projects.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    [
+        "title" => "My first project",
+        "description" => "Hey, this is my first project, do you like it?",
+        "image" => "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+        "topic" => "Web development"
+    ],
+    [
+        "title" => "My second project",
+        "description" => "Hey, this is my second project, do you like it?",
+        "image" => "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+        "topic" => "Database development"
+    ],
+    [
+        "title" => "My third project",
+        "description" => "Hey, this is my third project, do you like it?",
+        "image" => "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+        "topic" => "Web development"
+    ],
+    [
+        "title" => "My fourth project",
+        "description" => "Hey, this is my fourth project, do you like it?",
+        "image" => "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+        "topic" => "Frontend development"
+    ]
+];
---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center">
+
+            @foreach ($projects as $project)
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">{{ $project->title }}</div>
+                        <div class="card-body">{{ $project->description }}</div>
+                        <a class="d-flex justify-content-center" href="{{ route('admin.projects.show', $project->id) }}"><img
+                                src="{{ $project->image }}" class="comics-img w-25 align-self-center"
+                                alt="{{ $project->title }}">
+                        </a>
+                        <div class="card-body">{{ $project->topic }}</div>
+                        <div class="d-flex justify-content-center">
+                            <a href="{{ route('admin.projects.show', $project->id) }}"
+                                class="btn btn-primary mx-2 mb-2">Show
+                                details</a>
+                            <a href="{{ route('admin.projects.edit', $project->id) }}"
+                                class="btn btn-info mx-2 mb-2">Edit</a>
+                            <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST"
+                                class="d-inline-block mx-2 mb-2">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-danger">
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            @endforeach
+
+
+        </div>
+    </div>
+@endsection
